fae41beab8753aca752726df335aae6d3a925f9a
marcelo oliveira <marcelo.r.oliveira@ebradi.com.br>
2021-11-16 18:49:05 -0300

    refactoring scripts

--- personal_tab_sso/personal-tab-sso-index/static/js/auth.js
+++ personal_tab_sso/personal-tab-sso-index/static/js/auth.js
@@ -17,8 +17,8 @@ $(document).ready(function () {
                 // Something else went wrong
             }
         });
-});
-
+    });
+    
 function requestConsent() {
     getToken()
         .then(data => {
